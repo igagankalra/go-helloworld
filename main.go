2199cea20f6b80fe40157c9281107d773f6c8b00
Gagan Kalra <gagandeep.singh@logrhythm.com>
2021-07-01 20:25:15 +0530

    some chagees and more endpoints: 0.0.3

--- main.go
+++ main.go
@@ -1,15 +1,73 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Hello World, from Gagan!")
+func status(w http.ResponseWriter, r *http.Request) {
+	log.Println("Accessed status() function")
+	str := `{
+        {"result":"OK - healthy"},
+        status=200,
+        mimetype='application/json'
+    }`
+
+	var obj map[string]interface{}
+	json.Unmarshal([]byte(str), &obj)
+
+	fmt.Fprint(w, str)
+
+}
+
+func author(w http.ResponseWriter, r *http.Request) {
+	log.Println("Accessed author() function")
+	str := `{"Author Name":"Gagan Kalra",
+    "Github": "@igagankalra(https://github.com/igagankalra)",
+    "Twitter": "@igagankalra(https://twitter.com/igagankalra)"
+}`
+
+	// var obj map[string]interface{}
+	// json.Unmarshal([]byte(str), &obj)
+
+	fmt.Fprint(w, str)
+
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	log.Println("Accessed Hello world! function")
+	fmt.Fprintf(w, "Hello World, from Gagan!")
+}
+
+func metrics(w http.ResponseWriter, r *http.Request) {
+	log.Println("Accessed metrics() function")
+	str := `{
+        {
+            "status": "Success",
+            "Code": 0,
+            "data": {
+                "UsrCount":120,
+                "UserCountActive":23
+            }
+        },
+        status=200,
+        mimetype='application/json'
+
+    }`
+
+	var obj map[string]interface{}
+	json.Unmarshal([]byte(str), &obj)
+
+	fmt.Fprint(w, str)
 }
 
 func main() {
-    http.HandleFunc("/", helloWorld)
-    http.ListenAndServe(":6111", nil)
+	http.HandleFunc("/", helloWorld)
+	http.HandleFunc("/metrics", metrics)
+	http.HandleFunc("/status", status)
+	http.HandleFunc("/author", author)
+	http.ListenAndServe(":6111", nil)
+
 }
